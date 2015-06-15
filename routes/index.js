7dd14d48787c07d731ad27f0b8f48dbc37af7a37
liukan <city.of.beijing@gmail.com>
2015-06-15 21:08:06 +0800

    token and key imported

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var wechat = require("wechat");
+var winston = require("winston");
+
+var config = {
+  token: 'lkiversonlka',
+  appid : 'wxe310cb2cc9fb774fe',
+  encodingAESKey : 'Y5dcvkBQSGFruNyScs6NrXOX7DQSpXD4XoGub4RvW1xd'
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use(express.query());
+
+router.use('/wechat', wechat(config, function(req, res, next){
+  var message = req.weixin;
+  winston.log('debug', message);
+  res.reply("山那边的朋友，我听到您的声音了！");
+}));
 
 module.exports = router;
