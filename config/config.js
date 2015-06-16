c39accf5cb2e80c6a7c597082594ea71f698685c
liukan <city.of.beijing@gmail.com>
2015-06-16 13:41:58 +0800

    add configuration api.
    now we can configure the app menu

--- config/config.js
+++ config/config.js
@@ -7,7 +7,8 @@ var winston = require("winston");
 
 function config(rootDir){
     var self = this;
-    var dataFile = rootDir + "/config/data.yaml";
+    self.configFolder = rootDir + "/config";
+    var dataFile = rootDir + "/config/config.yaml";
     try{
         self.data = yaml.safeLoad(fs.readFileSync(dataFile, "utf-8"));
     }catch(e){
@@ -16,4 +17,17 @@ function config(rootDir){
     }
 };
 
+config.prototype.loadMenu = function(){
+    var self = this;
+    if(self.data.menu){
+        try{
+            var content = fs.readFileSync(self.configFolder + "/" + self.data.menu, "utf-8");
+            return JSON.parse(content);
+        }catch(error){
+            throw error;
+        }
+    }else{
+        throw new Error("no menu configuration");
+    }
+}
 exports.config = config;
