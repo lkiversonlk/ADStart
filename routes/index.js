bf065ed54efbdf63e773ce56d046aa6eaaf97b8b
lkiversonlk <city.of.beijing@gmail.com>
2015-06-18 16:46:46 +0800

    update about us message

--- routes/index.js
+++ routes/index.js
@@ -33,8 +33,8 @@ router.use('/wechat', wechat(app_config.data).text(function (message, req, res, 
 }).event(function (message, req, res, next) {
   switch (message.EventKey){
     case "aboutus":
-          res.reply("我们是位于北京市朝阳区的一家初创互联网广告公司，我们相信，互联网时代也将深刻的改变广告行业，广告需要更可信，更真实。我们会帮助好的产品想病毒一样传播开来。" +
-              "\n联系我们：\n 手机：18810232704 \n邮件： jerry.kliu@vip.126.com \n微信: 272293041");
+          res.reply("我们是位于北京市朝阳区的一家初创互联网广告公司，我们相信，互联网时代也将深刻的改变广告行业。移动时代的广告需要更可信，更真实，更亲切。\n我们会帮助好的产品想病毒一样传播开来。" +
+              "\n联系我们：\n手机:18810232704 \n邮件:jerry.kliu@vip.126.com \n微信:272293041");
   }
 }).device_text(function (message, req, res, next) {
   // TODO
