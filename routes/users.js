69edc92aef95103a44732ecf2fe5680bc2e7c87f
lkiversonlk <city.of.beijing@gmail.com>
2015-06-20 14:35:57 +0800

    Add oauth function

--- routes/users.js
+++ routes/users.js
@@ -17,17 +17,17 @@ router.get('/', function(req, res, next) {
 router.get("/:user_id", function(req, res, next){
   if(req.params.user_id == 'mine'){
     var code = req.query.code;
-    res.write("code : " + code);
     var oauth = req.app.get("wechat-oauth");
     oauth.getAccessToken(code, function(err, result){
       var accessToken = result.data.access_token;
-      res.write("access_token: " + accessToken);
       var openid = result.data.openid;
-      res.write("openid: " + openid);
       oauth.getUser(openid, function(err, result){
-        res.write(JSON.stringify(result));
+        if(err){
+          res.end("sorry, we can't get your infomation");
+        }else{
+          res.end("Hello, " + result.nickname)
+        }
       });
-      res.end();
     });
   }else{
     res.end();
