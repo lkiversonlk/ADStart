d5aeb47b8d8e74a4d6071bfc513d1dbb30af609c
lkiversonlk <city.of.beijing@gmail.com>
2015-06-22 23:53:55 +0800

    start upload process

--- public/templates/base.js
+++ public/templates/base.js
@@ -10,15 +10,21 @@ wx.config(config);
 wx.ready(function(){
     $("#start_upload").append("<br/> 请点击本框选择图片")
     $("#start_upload").on("click", function(){
-        $("#start_upload").hide();
-        $("#confirm_upload").show();
         wx.chooseImage({
             success: function(res){
                 var localIds = res.localIds;
                 var imgs = $(".dyr_markup");
-                $(imgs[0]).attr("src", localIds[0]);
-                $("#start_upload").hide();
-                $("#confirm_upload").show();
+
+                if(localIds.length != imgs.length){
+                    alert("您上传的照片数量与该广告要求的数量不符。");
+                    return;
+                }else{
+                    imgs.forEach(function(img, i){
+                        $(img).attr("src", localIds[i]);
+                    });
+                    $("#start_upload").hide();
+                    $("#confirm_upload").show();
+                }
             }
         });
     });
